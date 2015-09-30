1619bfd8ec7d5981f325a8e4f0929b6db4de1e08
zhang.jie <zhang.jie@integrasco.com>
2015-09-30 17:14:34 +0800

    add a script

---
+++ check_server.py
@@ -0,0 +1,23 @@
+import os
+import psutil
+
+
+def check_server_disk():
+    path = '/home/jie/'
+    st = os.statvfs(path)
+    free = st.f_bfree/st.f_blocks * 100
+    print(free)
+
+
+def check_cpu():
+    print(psutil.virtual_memory())
+    print(psutil.net_io_counters(pernic=True))
+
+
+def run():
+    check_server_disk()
+    check_cpu()
+
+
+if __name__ == '__main__':
+    run()
